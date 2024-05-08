54e642850ce38acc15824fa3f9a66ca5bb683f03
unknown <oojumu@yahoo.com>
2024-05-08 11:47:17 +0100

    first commit

---
+++ src/Projects.jsx
@@ -0,0 +1,58 @@
+
+import { useFetchProjects } from  './fetchProjects';
+
+const Projects = () => {
+
+    const {loading, projects } = useFetchProjects();
+
+    console.log(loading, projects);
+    if (loading)
+    {
+ 
+          return (
+          <section className='projects'> 
+                 <h3>Loading....</h3>
+          </section>);
+    }
+
+
+    return(
+        <section>
+            <div className='projects'>
+                <div className='title'>
+                    <h2>Projects</h2>
+                </div>
+
+                    <div className='title-underline'>
+                        
+                    </div>
+
+                <div className='projects-center'>
+                {projects.map((project) => {
+                const {id, img, url, title} = project;
+                return (
+                    <a
+                        key={id}
+                        href={url}
+                        target='_blank'
+                        rel='noreferrer'
+                        className='project'>
+                        
+
+                        <img src={img} alt={title} className='img' />
+                        <h5>{title}</h5>
+                    </a>
+                );
+            })}
+                </div>
+            </div>
+        </section>
+    )
+
+
+
+};
+
+
+export default Projects
+
---
+++ src/fetchProjects.jsx
@@ -0,0 +1,54 @@
+//import React from 'react';
+import { useState,useEffect } from 'react';
+import { createClient }  from 'contentful';
+
+const client = createClient({
+    space: '5p9hrewyh9hf',
+    environment: 'master',
+    accessToken: import.meta.env.VITE_API_KEY,
+});
+
+
+
+export const useFetchProjects = () => {
+
+    const [loading, setLoading] = useState(true)
+    const [projects, setProjects] = useState([])
+
+    const getData = async() =>{
+        try {
+            
+            const response = await client.getEntries({content_type: 'projecto' });
+            const projectoz = response.items.map((item) =>
+            {
+                const {title, url, image} = item.fields
+                const id = item.sys.id 
+                const img = image?.fields?.file?.url;
+                return { title, url, id, img };
+            });
+
+            setProjects(projectoz);
+            setLoading(false);
+        } 
+        catch (error) 
+        {
+                console.log(error);
+                setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    return {loading, projects }
+};
+
+
+
+
+
+
+
+
+//.then((response) =>  console.log(response));
